1a098b61a05bc3d8063fc43d8901267bfea723ac
Chrixie <Chrixie@hotmail.com>
2026-07-22 16:09:31 +0200

    .

---
+++ Tutorial 06/Exersice_07.cpp
@@ -0,0 +1,71 @@
+#include "stdio.h"
+
+int main()
+{
+	////-------------------------------------------------IF STATEMENTS---------------------------------------------
+	//int day;
+
+	//printf("Tell me a number 1, 2, 3, 5, 6, 7, 8 \n");
+	//scanf_s("%d", &day);
+
+	//if (day == 1 || day == 3 || day == 5 || day == 6 || day == 8)
+	//{
+	//	printf("Weekday");
+	//}
+
+	//else if (day == 2 || day == 7)
+	//{
+	//	printf("Weekend");
+	//}
+	//else
+	//	printf("Wrong number input");
+	////-------------------------------------------------IF STATEMENTS---------------------------------------------
+
+
+	////-------------------------------------------------SWITCH STATEMENT------------------------------------------
+	//int day;
+
+
+	//printf("Tell me a number 1, 2, 3, 5, 6, 7, 8 \n");
+	//scanf_s("%d", &day);
+
+	//switch (day)
+	//{
+	//case 1:
+	//	printf("Weekday");
+	//	break;
+	//case 2:
+	//	printf("Weekend");
+	//	break;
+	//case 3:
+	//	printf("Weekday");
+	//	break;
+	//case 5:
+	//	printf("Weekday");
+	//	break;
+	//case 6:
+	//	printf("Weekday");
+	//	break;
+	//case 7:
+	//	printf("Weekend");
+	//	break;
+	//case 8:
+	//	printf("Weekday");
+	//	break;
+	//default:
+	//	printf("Wrong number input");
+	////-------------------------------------------------SWITCH STATEMENT------------------------------------------
+
+
+	}
+	return 0;
+}
+
+
+/*
+An integer variable contains a value of 1 that represents Monday, 3 for Tuesday, 5 for Wednesday, 6 for Thursday, 8 for Friday, 2 for Saturday and 7 for Sunday.
+
+Design and write a program using if() statements to input a number into this variable and output "weekday" and "weekend" as appropriate.
+Design and write the same program using switch statements.
+Explain in your logbook which you find is the best solution, and why. Can you think of a better solution?
+*/
